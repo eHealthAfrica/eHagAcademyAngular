dfafe7992c1103052dbf8eeffc6610832ce0cdca
douno <douno30@gmail.com>
2016-08-11 23:29:19 +0000

    Initial commit

---
+++ patients.js
@@ -0,0 +1,5 @@
+[
+  {"pid":"1", "first_name":"John", "last_name":"Doe", "age":30},
+  {"pid":"2", "first_name":"Samuel", "last_name":"Mesfin", "age":100},
+  {"pid":"3", "first_name":"Moussa", "last_name":"Traore", "age":2}
+]
